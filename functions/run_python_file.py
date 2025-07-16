e4fa0720e7151dbd436d41d8cdf161906729c250
Alisher Nildibayev <alisher.nil@gmail.com>
2025-07-16 12:35:26 +0500

    refactor: update file validation logic in run_python_file for improved clarity and error handling

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,22 +1,34 @@
-from functions.common import ERROR_TEMPLATE
+from functions.config import ERROR_TEMPLATE
 from functions.exceptions import NotAPythonFile, OutsideWorkDirException
-from functions.utils import check_if_python_file, verify_filepath_is_valid_and_exists
+from functions.utils import resolve_file_path
+from functions.validators import (
+    file_exists,
+    file_is_python,
+    path_within_bounds,
+)
 
 
 def run_python_file(working_directory: str, file_path: str, args: list = []):
-    error_message = ""
     try:
-        full_path = verify_filepath_is_valid_and_exists(working_directory, file_path)
-        check_if_python_file(full_path)
+        full_file_path = validate_file_path(working_directory, file_path)
+    except Exception as e:
+        return ERROR_TEMPLATE.format(error=str(e))
+
+
+def validate_file_path(working_directory: str, file_path: str):
+    full_file_path = resolve_file_path(working_directory, file_path)
+    working_directory_path = resolve_file_path(working_directory)
+    try:
+        path_within_bounds(full_file_path, working_directory_path)
+        file_exists(full_file_path)
+        file_is_python(full_file_path)
     except OutsideWorkDirException:
-        error_message = (
+        raise Exception(
             f'Cannot execute "{file_path}" as it is '
             "outside the permitted working directory"
         )
     except FileNotFoundError:
-        error_message = f'File "{file_path}" not found.'
+        raise Exception(f'File "{file_path}" not found.')
     except NotAPythonFile:
-        error_message = f'"{file_path}" is not a Python file.'
-    finally:
-        if error_message:
-            return ERROR_TEMPLATE.format(error_message)
+        raise Exception(f'"{file_path}" is not a Python file.')
+    return full_file_path
