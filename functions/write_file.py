ffee03398d2fe460836c3d3431320bbf2cf4ad39
Alisher Nildibayev <alisher.nil@gmail.com>
2025-07-16 12:00:27 +0500

    refactor: update write_file function to improve error handling and streamline path validation

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,27 +1,32 @@
-from functions.common import ERROR_TEMPLATE
+from functions.config import ERROR_TEMPLATE
 from functions.exceptions import OutsideWorkDirException
 from functions.utils import (
     create_directory_if_missing,
-    verify_filepath_is_valid,
+    resolve_file_path,
 )
+from functions.validators import path_within_bounds
 
 
 def write_file(working_directory: str, file_path: str, content: str):
-    error_message = ""
     try:
-        full_file_path = verify_filepath_is_valid(working_directory, file_path)
+        full_file_path = validate_file_path(working_directory, file_path)
         create_directory_if_missing(full_file_path)
         with open(full_file_path, "w") as output_file:
             output_file.write(content)
+    except Exception as e:
+        return ERROR_TEMPLATE.format(error=e)
 
+    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+def validate_file_path(working_directory: str, file_path: str) -> str:
+    full_file_path = resolve_file_path(working_directory, file_path)
+    working_directory_path = resolve_file_path(working_directory)
+    try:
+        path_within_bounds(full_file_path, working_directory_path)
     except OutsideWorkDirException:
-        error_message = (
+        raise Exception(
             f'Cannot write to "{file_path}" as it is outside '
             "the permitted working directory"
         )
-    except Exception as e:
-        error_message = str(e)
-    finally:
-        if error_message:
-            return ERROR_TEMPLATE.format(error=error_message)
-    return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    return full_file_path
