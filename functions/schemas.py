aff8dc4b84c6eb8dff960c1cce830a52d01b52fc
Alisher Nildibayev <alisher.nil@gmail.com>
2025-07-21 18:03:04 +0500

    feat: implement schema for get_file_content and run_python_file functions

--- functions/schemas.py
+++ functions/schemas.py
@@ -17,9 +17,34 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
-# schema_get_file_content = types.FunctionDeclaration(
-#     name="get_file_content",
-#     description=
-# )
-# schema_run_python_file
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads file content. If the file is longer than maximum "
+    "allowed length the content is truncated.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read from, relative to the "
+                "working directory",
+            )
+        },
+    ),
+)
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs the python file. Execution is aborted if the program "
+    "takes longer than 30 seconds to run.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to run from, relative to the "
+                "working directory",
+            )
+        },
+    ),
+)
 # schema_write_file
